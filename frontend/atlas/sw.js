b9866b15aed2cad306840a5e326bb03efee660bf
Felipe Queiroz <mitaovlog00@gmail.com>
2026-06-02 22:11:53 -0300

    Style Atlas sidebar toggle

--- frontend/atlas/sw.js
+++ frontend/atlas/sw.js
@@ -1,5 +1,5 @@
 /* ===== ATLAS SERVICE WORKER ===== */
-const CACHE_VERSION = 'atlas-v2.1.6';
+const CACHE_VERSION = 'atlas-v2.1.7';
 const CORE_ASSETS = [
   '/atlas', '/atlas/', '/atlas/index.html',
   '/atlas/css/app.css',
